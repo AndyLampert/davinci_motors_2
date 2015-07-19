db8360013b3513685832a227c9d955a2b5b8c8e4
Andy Lampert <lamperta@gmail.com>
2015-07-19 17:40:50 -0600

    through slide 29

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,9 +1,28 @@
 class CarsController < ApplicationController
-  def index
+  before_action :set_car
 
+  def index
   end
 
   def new
+    @car = Car.new
+  end
 
+  def create
+    @car = Car.new(car_params)
+    creation_message = "#{@car.year} #{@car.make} #{@car.model} created"
+    if @car.save
+      redirect_to :cars_path,
+                  :notice => creation_message
+    end
+  end
+
+  private
+  def set_car
+    @user = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit([:year, :make, :model, :price])
   end
 end
